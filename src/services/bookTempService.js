ea56419808083485f2db64df3f71de2758f8f749
robb <90488725+manhnguyen1515@users.noreply.github.com>
2023-06-25 14:48:03 +0700

    Accept & refuse for publish book

--- src/services/bookTempService.js
+++ src/services/bookTempService.js
@@ -17,8 +17,38 @@ const getListBookTemps = async () => {
   }
 };
 
+const acceptPublishBook = async (bookTemp) => {
+  try {
+    const res = await axios.post(`/book-temps/accept`, bookTemp);
+    if (res.status === 200) {
+      return res.data;
+    }
+
+    return null;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const refusePublishBook = async (bookTemp) => {
+  try {
+    const res = await axios.post(`/book-temps/refuse`, bookTemp);
+    if (res.status === 200) {
+      return res.data;
+    }
+
+    return null;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
 const BookTempService = {
   getListBookTemps,
+  acceptPublishBook,
+  refusePublishBook,
 };
 
 export default BookTempService;
